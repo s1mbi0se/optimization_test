68ef6110a01a61096139b4907a1eaaafd75e84e9
Joao Medeiros <joao@simbioseventures.com>
2018-08-18 14:01:24 -0300

    Add new test cases

---
+++ src/main/java/TestCaseGenerator.java
@@ -0,0 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Random;
+
+public class TestCaseGenerator {
+
+    private static final int LEVEL = 3;
+    private static final Path TESTCASE_DIR = new File("testcases/level" + LEVEL).toPath();
+    private static final int[] QUANTITY_LEVELS = new int[]{500, 1000, 6000, 10000};
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) throws IOException {
+        if (!TESTCASE_DIR.toFile().exists()) {
+            TESTCASE_DIR.toFile().mkdirs();
+        }
+
+        final BufferedWriter writer = new BufferedWriter(new FileWriter(TESTCASE_DIR + "/input.txt", false));
+
+        final File inputFile = TESTCASE_DIR.resolve("input.txt").toFile();
+        final File expectedOutputFile = TESTCASE_DIR.resolve("expected.txt").toFile();
+
+        final StringBuilder stringBuilder = new StringBuilder();
+        int quantityLevel = QUANTITY_LEVELS[LEVEL];
+        for (int i = 0; i < quantityLevel; i++) {
+            for (int i1 = 0; i1 < 2; i1++) {
+                final int quantityCase = 10 + RANDOM.nextInt(quantityLevel / 5);
+                stringBuilder.setLength(0);
+                for (int i2 = 0; i2 < quantityCase; i2++) {
+                    final int value = RANDOM.nextInt(quantityLevel * 10);
+                    stringBuilder.append(value).append(",");
+                }
+                stringBuilder.append("\n");
+                writer.append(stringBuilder);
+            }
+        }
+
+        writer.close();
+
+        final IProcessor processor = new MyProcessor();
+        processor.process(inputFile, expectedOutputFile);
+    }
+
+}
